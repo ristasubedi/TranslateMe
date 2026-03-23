460f567360ba610467bf986e082968b023a55e74
ristasubedi <rsubedi536@gmail.com>
2026-03-23 12:05:15 -0700

    Initial Commit

---
+++ TranslateMe/TranslateMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslateMeApp.swift
+//  TranslateMe
+//
+//  Created by Ujjwal Adhikari on 3/23/26.
+//
+
+import SwiftUI
+
+@main
+struct TranslateMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
